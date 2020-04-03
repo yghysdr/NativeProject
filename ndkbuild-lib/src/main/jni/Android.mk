7b9fa2bb439f721af9038c7d0d7f79106320a5af
yghysdr <yghysdr@163.com>
2020-04-03 12:07:49 +0800

    ndk-build 支持so支持打包多个文件

--- ndkbuild-lib/src/main/jni/Android.mk
+++ ndkbuild-lib/src/main/jni/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH :=$(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := ndk-lib
-LOCAL_SRC_FILES :=native-lib.cpp
+LOCAL_SRC_FILES :=native-lib.cpp \ libs/child-lib.cpp
 
 include $(BUILD_SHARED_LIBRARY)
--- ndkbuild-lib/src/main/jni/native-lib.cpp
+++ ndkbuild-lib/src/main/jni/native-lib.cpp
@@ -1,9 +1,11 @@
 #include <jni.h>
 #include <string>
+#include "libs/child-lib.h"
 
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_tal_ndkbuild_HelloNdkBuild_getNativeString(JNIEnv *env, jobject thiz) {
-    return env->NewStringUTF("this is Java_com_tal_ndkbuild_HelloNdkBuild_getNativeString");
+    Child child;
+    return env->NewStringUTF(child.getKey().c_str());
 }
